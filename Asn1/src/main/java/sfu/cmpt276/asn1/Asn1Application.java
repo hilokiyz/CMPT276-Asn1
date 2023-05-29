11782b909a3ab6454e327ce5740c0ed405f87114
hilokiyz <74848293+hilokiyz@users.noreply.github.com>
2023-05-28 18:24:18 -0700

    Rebuilt Springboot files

--- Asn1/src/main/java/sfu/cmpt276/asn1/Asn1Application.java
+++ Asn1/src/main/java/sfu/cmpt276/asn1/Asn1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Asn1Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Asn1Application.class, args);
 	}
